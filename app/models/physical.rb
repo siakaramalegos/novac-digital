8d46f5d5ff0fb4354a3df378deef689cc66475cd
Sia <siakaramalegos@gmail.com>
2015-07-17 10:53:30 -0500

    Added data validation

--- app/models/physical.rb
+++ app/models/physical.rb
@@ -3,4 +3,6 @@ class Physical < ActiveRecord::Base
   belongs_to :tape_format
   belongs_to :brand
   belongs_to :condition
+
+  validates :owner_id, :tape, :title, :label_title, :brand_id, :condition_id, :tape_format_id, presence: true
 end
