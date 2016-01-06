0d72bd6cbbd27f36fc0013bee77f495633bcf45d
Sia <siakaramalegos@gmail.com>
2016-01-06 09:37:21 -0600

    remove serial links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :filmmakers
   resources :owners
-  resources :serials
+  # resources :serials
   root to: 'visitors#index'
   devise_for :users
   resources :users
